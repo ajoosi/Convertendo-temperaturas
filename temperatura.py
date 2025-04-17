ae50e88d24db0d17d391f8091d96233d3627d1aa
ajoosi <josideandrade1@hotmail.com>
2025-04-17 08:13:36 -0300

    Refaturação em temperaturas

--- temperatura.py
+++ temperatura.py
@@ -1,14 +1,19 @@
-c = int(input(' Informe a temperatura em °C:'))
-f = ((9*c)/5) + 32
-print('A temperatura de {} °C corresponde a {} °F!'.format(c,f))
-
+celsius = int(input("Informe a temperatura em °C: "))
+fahrenheit = ((9*celsius)/5) + 32 # A formula Fahrenheit é 9x celsius, dividido por 5, mais 32.
+print(f"A temperatura de {celsius} °C corresponde a {fahrenheit} graus Fahrenheit !")
 
 print ('.')
 print ('.')
 print ('.')
 
-fa = int(input(' Informe a temperatura em °F:'))
-ce = ((fa-32)*5) /9
-print('A temperatura de {} °F corresponde a {:.1f} °C!'.format(fa,ce))
+fa = int(input(' Informe a temperatura em °F: '))
+ce = ((fa-32)*5) /9 # A formula Celsius é Fahrenheit - 32, multiplicxado por 5, dividido por 9
+ce_int = int(ce)
+print(f"A temperatura de {fa} °F corresponde a {ce_int} graus celsius!")
+
+print ('.')
+print ('.')
+
+
 
 
